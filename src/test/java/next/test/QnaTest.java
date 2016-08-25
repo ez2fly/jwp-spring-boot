07ac7105c713e95ade2cc3c785970fda0dce155c
ez2fly <ez2fly@naver.com>
2016-08-25 19:27:12 +0900

    Test 코드 안되는거 문의해서 답변 얻음

--- src/test/java/next/test/QnaTest.java
+++ src/test/java/next/test/QnaTest.java
@@ -9,8 +9,14 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import next.config.DBInitializer;
+import next.repository.AnswerRepository;
+import next.repository.QuestionRepository;
+import next.repository.UserRepository;
 import next.service.QnaService;
 
+// 아래 테스트코드는 동작이 안됨.
+// DataJpaTest 는 jpa 로 생성되는 repository 만 불러와서 테스트 할수 있다함 
+// 요 경우에는 Mockito 로 해서 테스트 한다고 하심
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class QnaTest {
@@ -19,7 +25,7 @@ public class QnaTest {
 	@Autowired
 	private DBInitializer dbinit;
 	@Autowired
-	private QnaService qna;
+	private QnaService qna;	
 	
 	@Test
 	public void deleteQuestion() {
